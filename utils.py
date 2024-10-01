e8f3474328bc3480bbb17c83c5f527e7cc051f3f
eltonss <eltonsarmanho@gmail.com>
2024-10-01 19:34:17 -0300

    Update Code: New Code to build Json

--- utils.py
+++ utils.py
@@ -25,8 +25,35 @@ def save_config(config, filename):
 def pretty_print_json(data):
     print(json.dumps(data, indent=4, sort_keys=True))
 
-def save_as_json(data, filename):
-    """Save data to a JSON file"""
+
+def load_existing_json(filename):
+    """Load existing JSON data from file if it exists"""
+    if os.path.exists(filename):
+        with open(filename, 'r') as json_file:
+            try:
+                return json.load(json_file)
+            except json.JSONDecodeError:
+                return []
+    return []
+
+
+def save_as_json(new_data, filename):
+    """Save new data to a JSON file without duplicating existing content"""
+    # Carregar dados existentes
+    existing_data = load_existing_json(filename)
+
+    # Garantir que novos dados não sejam duplicados
+    # Aqui estou assumindo que cada "dado" é um dicionário único com uma chave exclusiva como 'id'
+    new_ids = {item['id'] for item in new_data}
+    combined_data = [item for item in existing_data if item['id'] not in new_ids] + new_data
+
+    # Salvar os dados combinados (existentes + novos) no arquivo
     with open(filename, "w") as json_file:
-        json.dump(data, json_file, indent=4, sort_keys=True)
-        print(f"Dados salvos em {filename} no formato JSON")
+        json.dump(combined_data, json_file, indent=4, sort_keys=True)
+        print(f"Dados atualizados e salvos em {filename} no formato JSON")
+
+#def save_as_json(data, filename):
+#    """Save data to a JSON file"""
+#    with open(filename, "w") as json_file:
+#        json.dump(data, json_file, indent=4, sort_keys=True)
+#        print(f"Dados salvos em {filename} no formato JSON")
